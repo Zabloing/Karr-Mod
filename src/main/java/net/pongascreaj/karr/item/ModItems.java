8a422c5d81be7d5bf55bf47a90b84d2522369a7d
Zabloing <zabloing894@gmail.com>
2021-11-10 22:07:41 +0100

    Bugfixes

--- src/main/java/net/pongascreaj/karr/item/ModItems.java
+++ src/main/java/net/pongascreaj/karr/item/ModItems.java
@@ -15,20 +15,28 @@ public class ModItems {
 
     public static final RegistryObject<Item> SQUID_FLESH = ITEMS.register("squid_flesh",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
     public static final RegistryObject<Item> SHEEP_MILK_BUCKET = ITEMS.register("sheep_milk_bucket",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
     public static final RegistryObject<Item> GOAT_MILK_BUCKET = ITEMS.register("goat_milk_bucket",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
     public static final RegistryObject<Item> OIL_BUCKET = ITEMS.register("oil_bucket",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
     public static final RegistryObject<Item> BLOOD_BUCKET = ITEMS.register("blood_bucket",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
-    public static final RegistryObject<Item> URINE_BUCKET = ITEMS.register("urine-bucket",
+
+    public static final RegistryObject<Item> URINE_BUCKET = ITEMS.register("urine_bucket",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
     public static final RegistryObject<Item> RICE = ITEMS.register("rice",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
     public static final RegistryObject<Item> SPINAL_BUCKET = ITEMS.register("spinal_bucket",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
     public static final RegistryObject<Item> ACID_BUCKET = ITEMS.register("acid_bucket",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
