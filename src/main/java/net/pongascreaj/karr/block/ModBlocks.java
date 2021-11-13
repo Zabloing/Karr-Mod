f828bef128b722e4d55074c710c8f5527a42e849
Zabloing <zabloing894@gmail.com>
2021-11-13 15:15:15 +0100

    Added charred bone, charred flesh, and skin. Added fat block.

--- src/main/java/net/pongascreaj/karr/block/ModBlocks.java
+++ src/main/java/net/pongascreaj/karr/block/ModBlocks.java
@@ -24,6 +24,21 @@ public class ModBlocks {
     //googic_bone
     public static final RegistryObject<Block> GOOGIC_BONE = registerBlock("googic_bone",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+    //fat_block
+    public static final RegistryObject<Block> FAT_BLOCK = registerBlock("fat_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+    //skin_block
+    public static final RegistryObject<Block> SKIN_BLOCK = registerBlock("skin_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+    //charred_googic_bone
+    public static final RegistryObject<Block> CHARRED_GOOGIC_BONE = registerBlock("charred_googic_bone",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+    //charred_flesh_block
+    public static final RegistryObject<Block> CHARRED_FLESH_BLOCK = registerBlock("charred_flesh_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+
+
+
 
     //block stuff
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
