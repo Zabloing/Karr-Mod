dc347ac8062d7a366ff1a5f3b825ea50de027170
Zabloing <zabloing894@gmail.com>
2021-11-14 22:53:21 +0100

    Bugfixes. Fixed axis orientation.

--- src/main/java/net/pongascreaj/karr/block/ModBlocks.java
+++ src/main/java/net/pongascreaj/karr/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,22 +21,22 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, Karr.MOD_ID);
     //flesh_block
     public static final RegistryObject<Block> FLESH_BLOCK = registerBlock("flesh_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+            () -> new Block(BlockBehaviour.Properties.of(Material.CLAY).strength(8f)));
     //googic_bone
     public static final RegistryObject<Block> GOOGIC_BONE = registerBlock("googic_bone",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
     //fat_block
     public static final RegistryObject<Block> FAT_BLOCK = registerBlock("fat_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(8f)));
     //skin_block
     public static final RegistryObject<Block> SKIN_BLOCK = registerBlock("skin_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(12f)));
     //charred_googic_bone
     public static final RegistryObject<Block> CHARRED_GOOGIC_BONE = registerBlock("charred_googic_bone",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
     //charred_flesh_block
     public static final RegistryObject<Block> CHARRED_FLESH_BLOCK = registerBlock("charred_flesh_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
+            () -> new Block(BlockBehaviour.Properties.of(Material.SAND).strength(8f)));
 
 
 
