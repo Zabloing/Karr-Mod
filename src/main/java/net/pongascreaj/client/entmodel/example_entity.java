2a3cafcfbbc989d3a86e3755de0b860d86ec0c23
Zabloing <zabloing894@gmail.com>
2021-11-27 19:13:05 +0100

    .

--- src/main/java/net/pongascreaj/client/entmodel/example_entity.java
+++ src/main/java/net/pongascreaj/client/entmodel/example_entity.java
@@ -13,7 +13,7 @@ import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
-public class custom_model<T extends Entity> extends EntityModel<T> {
+class custom_model<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "custom_model"), "main");
 	private final ModelPart bone;
